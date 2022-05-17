306407894c5feae4c3e2d64c7edfc77772ca2628
Alexey <61086054+AlexeyKuznetsovGit@users.noreply.github.com>
2022-05-17 12:18:31 +0700

    Rework

--- laba2.hs
+++ laba2.hs
@@ -1,7 +1,7 @@
-f :: ([Double] -> Double -> Double -> [Double]) -> ([Double],[Double]) -> Double -> Double -> [Double]
-f f2 (xs, ys) k m = f2 xs k m ++ f2 ys k m
+f2 :: ([Double] -> Double -> Double -> [Double]) -> [[Double]] -> Double -> Double -> [[Double]]
+f2 f3 xs k m = [f3 x k m | x<-xs]
 
-f2 :: [Double] -> Double -> Double -> [Double]
-f2 [] _ _ = []
-f2 (x : xs) k m = if x >= 0 then ((x*(-1))/k) : f2 (xs) k m
-         else ((x*(-1))*m) : f2 (xs) k m
+f3 :: [Double] -> Double -> Double -> [Double]
+f3 [] _ _ = []
+f3 (x : xs) k m = if x >= 0 then ((x*(-1))/k) : f3 (xs) k m
+         else ((x*(-1))*m) : f3 (xs) k m
